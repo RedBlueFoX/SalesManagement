1848c47a31599850e569e3967f127a75f4f5864b
RedBlueFoX <39486202+RedBlueFoX@users.noreply.github.com>
2021-04-27 10:18:54 +0900

    Добавьте файлы проекта.

---
+++ SalesManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
